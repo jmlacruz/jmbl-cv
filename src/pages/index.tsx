304829cb4d23f79b37e73ae3cc8b798a725839b0
JMBL <jmlacruz7@gmail.com>
2023-03-22 10:27:25 -0300

    Add font v7

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,7 +74,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2>
-              Learn <span>-&gt;</span>
+              NBA <span>-&gt;</span>
             </h2>
             <p>
               Learn about Next.js in an interactive course with&nbsp;quizzes!
